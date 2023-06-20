28e9959c1a3eae192c7a26019ca3a378ae0a6996
Paul <chorkunxin@gmail.com>
2023-06-20 10:05:04 +0800

    Added display artists

--- index.js
+++ index.js
@@ -4,16 +4,66 @@ const dotenv = require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const app = express();
+
+//  enable static files 
 app.use(express.static('public'));
+
+// enable form processing
 app.use(express.urlencoded({
     extended: false
 }));
+
+// set the view engine
 app.set('view engine', 'ejs');
 
-app.get('/', function(req,res){
-    res.send("Hello world");
-})
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+}
 
-app.listen(process.env.port || 3000, function(){
+async function main() {
+    // mysql.createConnection is an asynchronous function
+    // 1. it takes a long time to finish
+    // 2. await allows us to tell JS to wait till the operation
+    // is done before going on to the next line
+    // 3. await can only be called in a function  marked as a async
+    const db = await mysql.createConnection(dbConfig);
+    console.log("database has been connected!");
+
+    // setup the routes
+    app.get('/', async function (req, res) {
+        try {
+            // db.query actuallys return an array
+            // the first element is the results (i.e the rows)
+            // the second element are meta-data
+            // instead of the following:
+            // const results = await db.query("SELECT * FROM artists");
+            // const artists = results[0];
+            // instead we can use destructuring:
+            const [artists] = await db.query("SELECT * FROM artists");
+
+            console.log(artists);
+
+            // If we use res.send on an array or an object, it will convert
+            // it to JSON
+            res.render("artists", {
+                "artists": artists
+            })
+
+        } catch (e) {
+            console.log("Error =>", e);
+            res.status(500);  // allows us to send back the response with a HTTP code
+            res.send('Internal Server Error');
+        }
+
+
+    })
+}
+
+main();
+
+app.listen(process.env.port || 3000, function () {
     console.log("server has started");
 })
